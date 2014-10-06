63dd547c0c6817140591ff0fa92281b21a7e8cee
gabry944 <gabry944@student.liu.se>
2014-10-06 15:55:37 +0200

    Signed-off-by: gabry944 <gabry944@student.liu.se>

--- scripts/audio-example.js
+++ scripts/audio-example.js
@@ -6,7 +6,7 @@ require([
 
   var startFFT = function() {  
   	// hämtar ljudet i realtid
-	var analyzer = audio.RealtimeAnalyzer.forPlayer(models.player);
+	var analyzer = audio.RealtimeAnalyzer.forPlayer(models.player, audio.BAND31);
 	
 	//varje gång en ny buffert av ljud är redo kommer denna funktion att kalllas med ljuddatat.
 	analyzer.addEventListener('audio', function(data){
@@ -14,7 +14,7 @@ require([
 		// Arrayen data.audio.spectrum.left innehåller frekvenserna i ljudet
 
 		//Eftersom frekvenserna är uppdelade på 511 band och vi bara vill ha 20st, så måste vi skala om dem så att de hamnar i en array på 20 element
-		//console.log(data.audio.spectrum.left.length); //här ser vi att antalet är 511
+		console.log(data); //här ser vi att antalet är 511
 		console.log(data.audio.spectrum.left); 
  			var bandsPerScaledBand = Math.floor(data.audio.spectrum.left.length / 20);  // antalet mindre band som hamnar i de 20 banden vi vill representera
 			var scaledSpectrum = [];  // vår array med band
@@ -26,15 +26,15 @@ require([
 				}
 				scaledSpectrum[i] = band / bandsPerScaledBand; 
 			}
-			console.log("band innan " + scaledSpectrum); 
+			//console.log("band innan " + scaledSpectrum); 
 			// normaliserar bandet mellan 0 och 40 och lägger in i vår array 
 			var ratio = 96/40; // 95 är maxvärdet 
-			console.log("Max värde " + ratio); 
+			//console.log("Max värde " + ratio); 
 			for (var k = 0; k < 20; k++)
 			{
 				scaledSpectrum[k] = scaledSpectrum[k]/ ratio;
 			}
-			console.log("band efter " + scaledSpectrum); 
+			//console.log("band efter " + scaledSpectrum); 
 			// här ska jag sicka arrayen scaledSpectrum[] till isabelle
 			changeBoxHeight(scaledSpectrum);
 	});
