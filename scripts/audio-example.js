2d7973c108f0b98db186e9f76acb2a460ba9170a
gabry944 <gabry944@student.liu.se>
2014-10-06 14:32:52 +0200

    Signed-off-by: gabry944 <gabry944@student.liu.se>

--- scripts/audio-example.js
+++ scripts/audio-example.js
@@ -13,9 +13,8 @@ require([
 
 		// Arrayen data.audio.spectrum.left innehåller frekvenserna i ljudet
 
-		//Om frekvenserna är uppdelade på fler band än vi vill ha (20st) så måste vi skala om dem så att de hamnar i en array på 20 element
-		//if(data.audio.spectrum.left.length > 20){
-			console.log(data.audio.spectrum.left.length);
+		//Eftersom frekvenserna är uppdelade på 511 band och vi bara vill ha 20st, så måste vi skala om dem så att de hamnar i en array på 20 element
+		//console.log(data.audio.spectrum.left.length); //här ser vi att antalet är 511
  			var bandsPerScaledBand = Math.floor(data.audio.spectrum.left.length / 20);  // antalet mindre band som hamnar i de 20 banden vi vill representera
 			var scaledSpectrum = [];  // vår array med band
 			for(var i = 0; i < 20; i++){
@@ -23,23 +22,17 @@ require([
 				for(var j = 0; j < bandsPerScaledBand; j++){
 					band += data.audio.spectrum.left[(i * bandsPerScaledBand) + j]; // läggen in värden i bandet
 				}
-				scaledSpectrum[i] = band / bandsPerScaledBand; // normaliserar bandet och lägger in i vår array
+				scaledSpectrum[i] = band / bandsPerScaledBand; 
+			}
+
+			// normaliserar bandet och lägger in i vår array
+			var ratio = Math.max.apply( Math, scaledSpectrum);
+			for (var k = 0; k < 20; k++)
+			{
+				scaledSpectrum[i] = scaledSpectrum[i]/ ratio;
 			}
 			// här ska jag sicka arrayen scaledSpectrum[] till isabelle
-			// LjudSpectrum = function() {
-   //  			return scaledSpectrum;
-			// }
 			changeBoxHeight(scaledSpectrum);
-		/*}
-
-		else {
-			//pjs.updateData(data.audio.spectrum.left);
-			// här ska jag sicka arrayen data.audio.spectrum.left till isabelle
-			function LjudSpectrum() {
-    			return scaledSpectrum[data.audio.spectrum.left];
-			}
-			return 0;
-		}*/
 	});
 };
 
