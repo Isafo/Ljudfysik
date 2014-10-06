241812b44eed9df29b1f58da304fdf754692acc0
Isafo <isabelle.forsman@gmail.com>
2014-10-06 15:54:09 +0200

    31 band istället för 20

--- scripts/audio-example.js
+++ scripts/audio-example.js
@@ -16,9 +16,9 @@ require([
 		//Eftersom frekvenserna är uppdelade på 511 band och vi bara vill ha 20st, så måste vi skala om dem så att de hamnar i en array på 20 element
 		//console.log(data.audio.spectrum.left.length); //här ser vi att antalet är 511
 		console.log(data.audio.spectrum.left); 
- 			var bandsPerScaledBand = Math.floor(data.audio.spectrum.left.length / 20);  // antalet mindre band som hamnar i de 20 banden vi vill representera
+ 			var bandsPerScaledBand = Math.floor(data.audio.spectrum.left.length / 30);  // antalet mindre band som hamnar i de 20 banden vi vill representera
 			var scaledSpectrum = [];  // vår array med band
-			for(var i = 0; i < 20; i++){
+			for(var i = 0; i < 30; i++){
 				var band = 0.;  // ett band i vår array
 				for(var j = 0; j < bandsPerScaledBand; j++){
 					// data.audio.spectrum.left ger värden mellan 0 och -96 därför gör jag en ful lösning och skriver såhär för att få en korekt representation mellan 0 och 96.
@@ -30,7 +30,7 @@ require([
 			// normaliserar bandet mellan 0 och 40 och lägger in i vår array 
 			var ratio = 96/40; // 95 är maxvärdet 
 			console.log("Max värde " + ratio); 
-			for (var k = 0; k < 20; k++)
+			for (var k = 0; k < 30; k++)
 			{
 				scaledSpectrum[k] = scaledSpectrum[k]/ ratio;
 			}
